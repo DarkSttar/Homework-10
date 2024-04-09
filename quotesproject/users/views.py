077c31e87a8556543136e18defeee30e0051a156
DarkSttar <llldarklllstarlll@gmail.com>
2024-04-09 06:57:58 +0300

    update css,html

--- quotesproject/users/views.py
+++ quotesproject/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render ,redirect
-
-from .forms import RegisterForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm, LoginForm
 
 def signupuser(request):
     if request.user.is_authenticated:
@@ -16,3 +18,21 @@ def signupuser(request):
     return render(request, 'users/signup.html',context= {'form':RegisterForm()})
 
 
+def loginuser(request):
+    if request.user.is_authenticated:
+        return redirect(to='quoteapp:main')
+    
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'],password=request.POST['password'])
+        if user is None:
+            messages.error(request,"Username or Password didn't match")
+            return redirect(to='users:login')
+        
+        login(request,user)
+        return redirect(to='quoteapp:main')
+
+    return render(request, 'users/login.html',context={"form":LoginForm()})
+@login_required
+def logoutuser(request):
+    logout(request)
+    return redirect(to='quoteapp:main')
